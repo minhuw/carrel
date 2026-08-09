b6a43f92bcb2807497162a9a360f00b5ea2df8c1
minhuw <wangmh15@gmail.com>
2026-08-09 11:40:59 +0800

    carrel: remove edit suggestion telemetry

--- src/vs/workbench/contrib/chat/browser/chatEditing/chatEditingModifiedFileEntry.ts
+++ src/vs/workbench/contrib/chat/browser/chatEditing/chatEditingModifiedFileEntry.ts
@@ -11,7 +11,6 @@ import { clamp } from '../../../../../base/common/numbers.js';
 import { autorun, derived, IObservable, ITransaction, observableValue, observableValueOpts, transaction } from '../../../../../base/common/observable.js';
 import { URI } from '../../../../../base/common/uri.js';
 import { TextEdit } from '../../../../../editor/common/languages.js';
-import { EditDeltaInfo } from '../../../../../editor/common/textModelEditSource.js';
 import { localize } from '../../../../../nls.js';
 import { IConfigurationService } from '../../../../../platform/configuration/common/configuration.js';
 import { IFileService } from '../../../../../platform/files/common/files.js';
@@ -21,10 +20,8 @@ import { editorBackground, registerColor, transparent } from '../../../../../pla
 import { IUndoRedoElement, IUndoRedoService } from '../../../../../platform/undoRedo/common/undoRedo.js';
 import { IEditorPane } from '../../../../common/editor.js';
 import { IFilesConfigurationService } from '../../../../services/filesConfiguration/common/filesConfigurationService.js';
-import { IAiEditTelemetryService } from '../../../editTelemetry/browser/telemetry/aiEditTelemetry/aiEditTelemetryService.js';
 import { ICellEditOperation } from '../../../notebook/common/notebookCommon.js';
 import { ChatUserAction, IChatService } from '../../common/chatService/chatService.js';
-import { isAgentHostSessionResource } from '../../common/chatSessionsService.js';
 import { ChatEditKind, IModifiedEntryTelemetryInfo, IModifiedFileEntry, IModifiedFileEntryEditorIntegration, ISnapshotEntry, ModifiedFileEntryState } from '../../common/editing/chatEditingService.js';
 import { IChatResponseModel } from '../../common/model/chatModel.js';
 
@@ -111,7 +108,6 @@ export abstract class AbstractChatEditingModifiedFileEntry extends Disposable im
 		@IFileService protected readonly _fileService: IFileService,
 		@IUndoRedoService private readonly _undoRedoService: IUndoRedoService,
 		@IInstantiationService protected readonly _instantiationService: IInstantiationService,
-		@IAiEditTelemetryService private readonly _aiEditTelemetryService: IAiEditTelemetryService,
 	) {
 		super();
 
@@ -270,53 +266,10 @@ export abstract class AbstractChatEditingModifiedFileEntry extends Disposable im
 	protected abstract _doReject(): Promise<void>;
 
 	protected _notifySessionAction(outcome: 'accepted' | 'rejected' | 'userModified') {
-		this._notifyAction({ kind: 'chatEditingSessionAction', uri: this.modifiedURI, hasRemainingEdits: outcome === 'userModified', outcome });
+		this._notifyAction({ kind: 'chatEditingSessionAction', uri: this.modifiedURI, hasRemainingEdits: false, outcome });
 	}
 
 	protected _notifyAction(action: ChatUserAction) {
-		const isAgentHostSession = isAgentHostSessionResource(this._telemetryInfo.sessionResource);
-		if (action.kind === 'chatEditingHunkAction' && action.outcome === 'accepted') {
-			this._aiEditTelemetryService.handleCodeAccepted({
-				suggestionId: undefined, // TODO@hediet try to figure this out
-				acceptanceMethod: 'accept',
-				presentation: 'highlightedEdit',
-				modelId: this._telemetryInfo.modelId,
-				modeId: this._telemetryInfo.modeId,
-				applyCodeBlockSuggestionId: this._telemetryInfo.applyCodeBlockSuggestionId,
-				editDeltaInfo: new EditDeltaInfo(
-					action.linesAdded,
-					action.linesRemoved,
-					-1,
-					-1,
-				),
-				feature: this._telemetryInfo.feature,
-				languageId: action.languageId,
-				source: undefined,
-				sourceRequestId: this._telemetryInfo.requestId,
-				isAgentHostSession,
-			});
-		} else if (action.kind === 'chatEditingHunkAction' && action.outcome === 'rejected') {
-			this._aiEditTelemetryService.handleCodeRejected({
-				suggestionId: undefined,
-				rejectionMethod: 'reject',
-				presentation: 'highlightedEdit',
-				modelId: this._telemetryInfo.modelId,
-				modeId: this._telemetryInfo.modeId,
-				applyCodeBlockSuggestionId: this._telemetryInfo.applyCodeBlockSuggestionId,
-				editDeltaInfo: new EditDeltaInfo(
-					action.linesAdded,
-					action.linesRemoved,
-					-1,
-					-1,
-				),
-				feature: this._telemetryInfo.feature,
-				languageId: action.languageId,
-				source: undefined,
-				sourceRequestId: this._telemetryInfo.requestId,
-				isAgentHostSession,
-			});
-		}
-
 		this._chatService.notifyUserAction({
 			action,
 			agentId: this._telemetryInfo.agentId,

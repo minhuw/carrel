a6ead07a1fa4ad8779ce83471dfd266d19e2c8a6
minhuw <wangmh15@gmail.com>
2026-08-10 13:57:20 +0800

    carrel: remove testing feature

--- build/npm/dirs.ts
+++ build/npm/dirs.ts
@@ -59,7 +59,6 @@ export const dirs = [
 	'test/scenario',
 	'test/mcp',
 	'.vscode/extensions/vscode-selfhost-import-aid',
-	'.vscode/extensions/vscode-selfhost-test-provider',
 	'.vscode/extensions/vscode-extras',
 	'.vscode/extensions/vscode-pr-pinger',
 ];

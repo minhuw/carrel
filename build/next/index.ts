e7b46f53142fa711351f050f1bc0c343d8ddb353
minhuw <wangmh15@gmail.com>
2026-08-11 09:34:22 +0800

    carrel: remove microsoft-authentication extension

--- build/next/index.ts
+++ build/next/index.ts
@@ -450,7 +450,6 @@ function readISODate(outDir: string): string {
 const desktopStandaloneFiles = [
 	'vs/base/parts/sandbox/electron-browser/preload.ts',
 	'vs/base/parts/sandbox/electron-browser/preload-aux.ts',
-	'vs/platform/browserView/electron-browser/preload-browserView.ts',
 ];
 
 async function compileStandaloneFiles(outDir: string, doMinify: boolean, target: BuildTarget): Promise<void> {
--- src/vs/workbench/api/browser/extensionHost.contribution.ts
+++ src/vs/workbench/api/browser/extensionHost.contribution.ts
@@ -42,7 +42,6 @@ import './mainThreadLanguages.js';
 import './mainThreadLogService.js';
 import './mainThreadMessageService.js';
 import './mainThreadManagedSockets.js';
-import './mainThreadBrowserTunnelProxy.js';
 import './mainThreadOutputService.js';
 import './mainThreadProgress.js';
 import './mainThreadQuickDiff.js';
@@ -80,7 +79,6 @@ import './mainThreadProfileContentHandlers.js';
 import './mainThreadDataChannels.js';
 import './mainThreadMeteredConnection.js';
 import './mainThreadGitExtensionService.js';
-import './mainThreadBrowsers.js';
 
 export class ExtensionPoints implements IWorkbenchContribution {
 
